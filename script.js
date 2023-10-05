d85c3de1bffd4eaf63bf4e0635939517fbe9d40e
diegooliveira7 <diegoolliveira1999@gmail.com>
2023-10-05 20:58:21 -0300

    correção do bug da sobreposição da home sobre head

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     navMenu.classList.toggle('ativo');
 });
+
+
+//Função digitando na home
+
+function actionLetter(element) {
+    const arrText = element.innerHTML.split('');
+    element.innerHTML = '';
+    arrText.forEach((letter, i) => {
+        setTimeout(()=> {
+            element.innerHTML += letter;
+        }, 75 * i);
+    });
+}
+
+const title = document.querySelector('.text-h1');
+actionLetter(title);
+
+
